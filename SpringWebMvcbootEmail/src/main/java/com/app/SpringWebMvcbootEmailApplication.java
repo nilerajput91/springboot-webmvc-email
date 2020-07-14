48623dcb34a47da56914cae2a31ec1e6ce9645b5
Nilesh Rajput <neelraj539@gmail.com>
2020-07-14 11:21:23 +0530

    Add files via upload
    Springboot web mvc email

---
+++ SpringWebMvcbootEmail/src/main/java/com/app/SpringWebMvcbootEmailApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvcbootEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMvcbootEmailApplication.class, args);
+	}
+
+}
---
+++ SpringWebMvcbootEmail/src/main/java/com/app/model/Product.java
@@ -0,0 +1,49 @@
+package com.app.model;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.data.annotation.Transient;
+
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name="producttab")
+public class Product {
+
+	@Id
+	@Column(name="pid")
+	@GeneratedValue
+	private Integer id;
+	
+	@Column(name="pcode")
+	private String code;
+	
+	@Column(name="pname")
+	private String name;
+	
+	@Column(name="pcost")
+	private Double cost;
+	
+	@Column(name="pgst")
+	private Integer gst;
+
+	@Column(name="pnote")
+	private String note;
+	
+	@Transient 
+	private String email;
+
+	public Integer getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getEmail() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
